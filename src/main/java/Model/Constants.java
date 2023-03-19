5bafbd100d552ca280816c6cc0f59fb8ab1e2ccc
MaximaKammerer <r.kurbanov.krd@yandex.ru>
2023-03-19 19:35:20 +0300

    add_Model_and_Methods

---
+++ src/main/java/Model/Constants.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Constants {
+    public final static String NAME = "Ganzalis";
+    public final static String PASSWORD = "123";
+    public final static String  EMAIL = "Ganzalis@yandex.ru";
+
+    public final static String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+
+
+}
